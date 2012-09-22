42c3d3d7d0af53893c08911bfcc9e97b8eb64211
Timothy J Fontaine <tjfontaine@gmail.com>
2012-09-22 19:46:27 -0400

    update client example

--- example.js
+++ example.js
@@ -1,9 +1,30 @@
-var purple = require('./purple');
+var Purple = require('./purple');
 
-var p = new purple({
+var p = new Purple({
   //debug: true,
 });
 
 p.protocols.forEach(function(prpl) {
   console.log(prpl.id, prpl.name);
+});
+
+var Status = require('./lib/status');
+var Account = require('./lib/account');
+
+var account = new Account('someluser', 'prpl-aim');
+account.password = 'somepassword';
+
+p.enableAccount(account);
+
+var s = new Status('Testing App', Status.AVAILABLE);
+p.savedstatus = s;
+
+p.on('create_conversation', function (conversation) {
+  console.log("we have a new conversation", conversation.title, conversation.name);
+});
+
+p.on('write_conv', function (conv, who, alias, message, flags, time) {
+  if (alias != 'nomegannomore') {
+    conv.send('you said "' + message + '"');
+  }
 });
